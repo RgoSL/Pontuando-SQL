ad0889b47cbdeb476ef5c673a1e652e6c4c3c0df
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2024-08-14 23:52:17 -0300

    Add files via upload

---
+++ Atividade Pontuando/Values Funcionarios.sql
@@ -0,0 +1,19 @@
+INSERT INTO dbo.Funcionarios
+([Codigo],[PrimeiroNome],[SegundoNome],[UltimoNome],[DataNasci],[CPF],[RG],[Endereco],[CEP],[UF],[Telefone],[Funcao],[Salario])
+VALUES
+( 1, N'Miguel', N'Yudi', N'Baba', '2007-04-10', '01123451299', '293221944', 'Jardim Robru', '02210000', 'SP', '5521979057356', N'Borracheiro', 1412),
+( 2, N'Ana', N'Carla', N'Martins', '1990-06-15', '12345678900', '456789123', 'Rua das Flores', '12345678', 'RJ', '21987654321', N'Recepcionista', 2500),
+( 3, N'Carlos', N'Velia', N'Silva', '1985-11-23', '98765432100', '321654987', 'Avenida Paulista', '01001000', 'GO', '11987654321', N'Gerente', 6000),
+( 4, N'Julia', N'Marques', N'Santos', '1992-03-12', '23456789012', '654987321', 'Praça da Sé', '01001001', 'SC', '11987654322', N'Assistente', 2200),
+( 5, N'Roberto', N'Costa', N'Oliveira', '1978-12-05', '34567890123', '789654123', 'Rua dos Bentos', '04020000', 'SP', '11987654323', N'Analista', 3200),
+( 6, N'Beatriz', N'Ribeiro', N'Gomes', '1989-07-19', '45678901234', '456123789', 'Rua das Palmeiras', '02010010', 'RJ', '21987654324', N'Coordenadora', 4000),
+( 7, N'Pedro', N'Paiva', N'Velasco', '1995-02-14', '56789012345', '321789654', 'Rua das Acácias', '01001010', 'SP', '11987654325', N'Técnico', 2800),
+( 8, N'Laura', N'Souza', N'Pereira', '1993-08-22', '67890123456', '987654321', 'Avenida das Nações', '03020020', 'MG', '31987654326', N'Contadora', 3400),
+( 9, N'Felipe', N'Vieira', N'Oliveira', '2007-06-28', '78901234567', '654321987', 'Inácio Monteiro', '04030030', 'SP', '42987654327', N'Diretor', 7500),
+(10, N'Claudia', N'Vega', N'Alves', '1991-01-25', '89012345678', '789456123', 'Rua dos Lírios', '05040040', 'BA', '71987654328', N'Especialista', 3900),
+(11, N'Gustavo', N'Henrique', N'Ribeiro', '1984-10-30', '90123456789', '321456789', 'Ponte Rasa', '06050050', 'SP', '62987654329', N'Assistente', 2100),
+(12, N'Mariana', N'Akemi', N'Santos', '1996-12-17', '01234567890', '654321456', 'Vila Nhocuné', '07060060', 'SP', '48987654330', N'Consultora', 5100),
+(13, N'João', N'Velosa', N'Ribeiro', '1982-05-09', '12345678901', '789321456', 'Rua dos Peixes', '08070070', 'RS', '51987654331', N'Administração', 2700),
+(14, N'Teresa', N'Conde', N'Pinto', '1994-11-01', '23456789012', '654987123', 'Rua dos Girassóis', '09080080', 'DF', '61987654332', N'Analista de Sistemas', 3300),
+(15, N'Rafael', N'Costa', N'Azevedo', '1988-06-18', '34567890123', '321654987', 'Rua da Aurora', '10090090', 'PR', '11987654333', N'Engenheiro', 4200);
+GO
